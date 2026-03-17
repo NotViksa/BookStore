bf7f977757325f188e68180aa72b07b1c1d5dcd5
NotViksa <viki.10019@gmail.com>
2026-03-17 22:25:39 +0200

    added SearchViewModel

---
+++ BookStore.Web/Models/SearchViewModel.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.Data.Models;
+
+namespace BookStore.Web.ViewModels
+{
+    public class SearchViewModel
+    {
+        public string SearchTerm { get; set; }
+
+        public IEnumerable<Book> Results { get; set; }
+
+        public int ResultCount { get; set; }
+
+        // Filters
+        public string? Genre { get; set; }
+
+        public string? Author { get; set; }
+
+        public decimal? MinPrice { get; set; }
+
+        public decimal? MaxPrice { get; set; }
+
+        // Sorting
+        public string SortBy { get; set; } = "relevance"; // relevance, price_low, price_high, newest, rating
+    }
+}
