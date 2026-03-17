0c71f08e58bbb91b246db3eaa47ea9e4035a6d8e
NotViksa <viki.10019@gmail.com>
2026-03-17 22:27:35 +0200

    added BooksByAuthorViewModel

---
+++ BookStore.Web/Models/BooksByAuthorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Data.Models;
+
+namespace BookStore.Web.ViewModels
+{
+    public class BooksByAuthorViewModel
+    {
+        public string Author { get; set; }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public int BookCount { get; set; }
+
+        public IEnumerable<string> Genres { get; set; }
+    }
+}
