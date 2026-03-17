9ce02f03a89a82153510574abf05f881e8fc5e12
NotViksa <viki.10019@gmail.com>
2026-03-17 22:26:42 +0200

    added WishlistViewModel

---
+++ BookStore.Web/Models/WishlistViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Data.Models;
+
+namespace BookStore.Web.ViewModels
+{
+    public class WishlistViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+
+        public int TotalItems { get; set; }
+
+        public decimal TotalValue => Books?.Sum(b => b.Price) ?? 0;
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+    }
+}
