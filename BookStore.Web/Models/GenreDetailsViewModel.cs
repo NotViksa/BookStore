5e8baf70acf89fdeeccc407ce37416569ae46bbd
NotViksa <viki.10019@gmail.com>
2026-03-17 22:24:48 +0200

    added GenreDetailsViewModel

---
+++ BookStore.Web/Models/GenreDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Data.Models;
+
+namespace BookStore.Web.ViewModels
+{
+    public class GenreDetailsViewModel
+    {
+        public Genre Genre { get; set; }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public int BookCount { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+    }
+}
